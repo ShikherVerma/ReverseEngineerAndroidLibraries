2b7cb429b56296eab752c0eb0fc1cbbd3eebf4c8
Shikher Verma <root@shikherverma.com>
2017-08-23 10:32:06 +0530

    Add ButterKnife sample

--- app/src/main/java/com/shikherverma/sampleapplication/MainActivity.java
+++ app/src/main/java/com/shikherverma/sampleapplication/MainActivity.java
@@ -1,13 +1,28 @@
 package com.shikherverma.sampleapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onButtonClick(View v) {
+        text.setText("Epoch :" + System.currentTimeMillis()/1000);
     }
 }
